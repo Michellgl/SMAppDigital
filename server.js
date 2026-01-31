65a645fd453802d6eea209b8f692a2452e14c652
Michell Gabriel Lucas <gabriellucas5342@gmail.com>
2026-01-30 19:40:26 -0600

    se coloco el panel del administrador, base de datos, y otras pruebas

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const session = require("express-session");
 const db = require("./database");
+
 
 const app = express();
 const PORT = 3000;
 
+
 app.use(express.json());
+app.use(session({
+    secret: "smapp-secret",
+    resave: false,
+    saveUninitialized: false
+}));
+
 app.use(express.static("./"));
 
 
@@ -25,6 +34,34 @@ app.post("/guardar-mensaje", (req, res) => {
 
 
 app.get("/mensajes", (req, res) => {
+    db.all("SELECT * FROM mensajes ORDER BY fecha DESC", [], (err, rows) => {
+        res.json(rows);
+    });
+});
+
+app.post("/admin-login", (req, res) => {
+    const { usuario, password } = req.body;
+
+    db.get(
+        "SELECT * FROM admin WHERE usuario = ? AND password = ?",
+        [usuario, password],
+        (err, row) => {
+            if (row) {
+                req.session.admin = true;
+                res.json({ ok: true });
+            } else {
+                res.json({ ok: false });
+            }
+        }
+    );
+});
+
+function authAdmin(req, res, next) {
+    if (req.session.admin) next();
+    else res.status(401).json({ error: "No autorizado" });
+}
+
+app.get("/admin-mensajes", authAdmin, (req, res) => {
     db.all("SELECT * FROM mensajes ORDER BY fecha DESC", [], (err, rows) => {
         res.json(rows);
     });
